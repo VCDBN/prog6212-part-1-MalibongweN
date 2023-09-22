e9df43bee387eeffc5835a1184f3d24302800dcc
lab_services_student <lab_services_student@lab000000>
2023-09-22 14:21:11 +0000

    Add project files.

---
+++ TimeManagementApplication/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using TimeManagementLibrary;
+
+namespace TimeManagementApplication
+{
+    public partial class MainWindow : Window
+    {
+        private TimeManagementHelper timeManagementHelper = new TimeManagementHelper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Get user input and call library methods
+                string code = CodeTextBox.Text;
+                string name = NameTextBox.Text;
+                int credits = int.Parse(CreditsTextBox.Text);
+                int classHoursPerWeek = int.Parse(ClassHoursTextBox.Text);
+
+                timeManagementHelper.AddModule(code, name, credits, classHoursPerWeek);
+
+                int numberOfWeeks = int.Parse(WeeksTextBox.Text);
+                DateTime startDate = StartDateDatePicker.SelectedDate ?? DateTime.Now;
+                timeManagementHelper.SetSemesterInfo(numberOfWeeks, startDate);
+
+                var moduleSummaries = timeManagementHelper.CalculateModuleSummaries();
+
+                // Update UI with the module summaries
+                ModuleListView.Items.Clear();
+
+                foreach (var summary in moduleSummaries)
+                {
+                    ModuleListView.Items.Add(summary);
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Invalid input. Please enter valid numeric values.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SubmitHoursButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string selectedCode = CodeTextBox.Text;
+                double hoursWorked = double.Parse(HoursWorkedTextBox.Text);
+                DateTime recordDate = RecordDateDatePicker.SelectedDate ?? DateTime.Now;
+
+                var modulesList = timeManagementHelper.GetModules();
+
+                var selectedModule = modulesList.FirstOrDefault(m => m.Code == selectedCode);
+
+                if (selectedModule != null)
+                {
+                    var recordedHour = new TimeManagementHelper.RecordedHour { Date = recordDate, Hours = hoursWorked };
+                    selectedModule.RecordedHours.Add(recordedHour);
+
+                    var moduleSummaries = timeManagementHelper.CalculateModuleSummaries();
+
+                    ModuleListView.Items.Clear();
+
+                    foreach (var summary in moduleSummaries)
+                    {
+                        ModuleListView.Items.Add(summary);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Module not found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Invalid input. Please enter valid numeric values.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ TimeManagementLibrary/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeManagementLibrary
+{
+    public class TimeManagementHelper
+    {
+        private List<Module> modules = new List<Module>();
+        private int numberOfWeeks;
+        private DateTime startDate;
+
+        public List<Module> GetModules()
+        {
+            return modules;
+        }
+
+        public void AddModule(string code, string name, int credits, int classHoursPerWeek)
+        {
+            // Create a new Module instance and add it to the modules list
+            modules.Add(new Module(code, name, credits, classHoursPerWeek));
+        }
+
+        public void SetSemesterInfo(int weeks, DateTime startDate)
+        {
+            // Set the semester information
+            numberOfWeeks = weeks;
+            this.startDate = startDate;
+        }
+
+        public List<ModuleSummary> CalculateModuleSummaries()
+        {
+            // Calculate the self-study hours per week based on module credits and weeks
+            double selfStudyHoursPerWeek = modules.Select(m => (m.Credits * 10.0) / numberOfWeeks - m.ClassHoursPerWeek).FirstOrDefault();
+
+            // Create module summaries for each module
+            var moduleSummaries = modules.Select(m => new ModuleSummary
+            {
+                Code = m.Code,
+                Name = m.Name,
+                Credits = m.Credits,
+                ClassHoursPerWeek = m.ClassHoursPerWeek,
+                SelfStudyHoursPerWeek = selfStudyHoursPerWeek,
+                RecordedHours = m.RecordedHours.Count(),
+                RemainingHours = selfStudyHoursPerWeek - m.RecordedHours.Count()
+            }).ToList();
+
+            return moduleSummaries;
+        }
+
+        public class Module
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public int Credits { get; set; }
+            public int ClassHoursPerWeek { get; set; }
+            public List<RecordedHour> RecordedHours { get; set; } = new List<RecordedHour>();
+
+            public Module(string code, string name, int credits, int classHoursPerWeek)
+            {
+                Code = code;
+                Name = name;
+                Credits = credits;
+                ClassHoursPerWeek = classHoursPerWeek;
+            }
+        }
+
+        public class RecordedHour
+        {
+            public DateTime Date { get; set; }
+            public double Hours { get; set; }
+        }
+
+        public class ModuleSummary
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public int Credits { get; set; }
+            public int ClassHoursPerWeek { get; set; }
+            public double SelfStudyHoursPerWeek { get; set; }
+            public int RecordedHours { get; set; }
+            public double RemainingHours { get; set; }
+        }
+    }
+}
